c276b093dfa76f85a2e8d833277b8d46cdade4ff
Ignacy <itjaniszewski@gmail.com>
2020-03-29 13:23:03 +0200

    First maps commit

---
+++ views/maps.php
@@ -0,0 +1,28 @@
+<body>
+	<div id="mapid"></div>
+
+		<!-- Modal -->
+		<div class="modal fade" id="modalpopup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+		  aria-hidden="true">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			  <div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Siema! Co dostarczysz?</h5>
+			  </div>
+			  <div class="modal-body">
+
+					  <p class="md-form mb-1">Ile przyłbic?</p>
+					  <input type="text" id="readyBascinetsNo">
+
+					  <p class="md-form mb-1">Ile materiałów?</p>
+					  <input type="text" id="MaterialsNeededNo">
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+				<button type="button" class="btn btn-primary" id="driver-confirmation">Potwierdź</button>
+			  </div>
+			</div>
+		  </div>
+		</div>
+
+</body>
